7f52850db281944d268738ff2328382fbc957561
Rachel Poonsiriwong <37454207+poonsiriwong@users.noreply.github.com>
2019-03-03 16:48:40 -0800

    Update concentric-circles.js

--- concentric-circles.js
+++ concentric-circles.js
@@ -2,9 +2,19 @@ function setup() {
   createCanvas(400, 400);
 }
 
+var x = [];
+var y = [];
+
 function draw() {
   background(220);
-  for (var i = 10; i <= 200; i = i + 10) {
-    circle(200, 200, i);
+	
+	x.push(200); // equivalent to append(x, mouseX)
+  y.push(200); // equivalent to append(y, mouseY)
+
+  for (var i = 0; i < x.length; i = i + 10) {
+    ellipse(x[i], y[i], 1 + (x.length - i));
   }
+
+  x = x.slice(-390); // keep the last 50 x values
+  y = y.slice(-390); // keep the last 50 y values
 }
