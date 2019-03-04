d1fa753378b35f9f6847d5ad1896791350daedd7
Rachel Poonsiriwong <37454207+poonsiriwong@users.noreply.github.com>
2019-03-03 16:52:46 -0800

    Update diamond_lines.js

--- diamond_lines.js
+++ diamond_lines.js
@@ -4,7 +4,10 @@ function setup() {
 
 function draw() {
   background(220);
-  for (var i = 10; i <= 390; i = i + 10) {
+  for (var i = 10; i <= 200; i = i + 10) {
     line(200 - i, i , 200 + i, i);
+  }
+  for (var i = 10; i <= 200; i = i + 10) {
+    line(200 - i, 400 - i , 200 + i, 400 - i);
   }
 }
